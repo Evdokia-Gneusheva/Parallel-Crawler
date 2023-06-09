de5e47c22ad4bd5c56ab571b9b02e1c4b15e9c1e
evdokia0 <dusya.gn@gmail.com>
2023-06-09 14:19:45 +0200

    Testing

--- crawler.h
+++ crawler.h
@@ -13,6 +13,7 @@
 #include <atomic>
 
 std::atomic<bool> has_level_been_reached(false);
+std::atomic<int> counter(0);
 
 struct CompareFirst
 {
@@ -41,7 +42,6 @@ struct WriteFunctionData
     StripedHashSet<Webpage> *HashSet;
     int current_level;
     bool only_local;
-    std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *checked_urls;
     std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *new_urls;
 };
 
@@ -71,7 +71,8 @@ bool compare_domains(const std::string &domain1, const std::string &domain2)
                       });
 }
 
-std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> extract_links(const std::string &receivedData, std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> &checked_urls, std::string root_url, bool only_local)
+std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> extract_links(const std::string &receivedData,
+                                                                                                std::string root_url, bool only_local, StripedHashSet<Webpage> *HashSet)
 {
     std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> links;
     std::regex link_pattern("<a[^>]*href=\"([^\"]*)\"[^>]*>");
@@ -81,6 +82,8 @@ std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst
     for (std::sregex_iterator it(receivedData.begin(), receivedData.end(), link_pattern), end_it; it != end_it; ++it)
     {
         std::string url = (*it)[1].str();
+
+
 
         if (url.find("http") == std::string::npos && url.find("https") == std::string::npos && url[0] == '/' && url[1] != '/')
         {
@@ -94,13 +97,12 @@ std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst
 
         std::string domain = extract_domain(url);
 
-        bool exists = std::any_of(checked_urls.begin(), checked_urls.end(),
-                                  [&](const std::pair<std::string, std::string> &pair)
-                                  {
-                                      return pair.first == url;
-                                  });
+        Webpage toInsert;
+        toInsert.url = url;
 
-        if (!only_local && !exists && url.find("index.php") == std::string::npos && !domain.empty())
+        bool exists = HashSet->contains(toInsert);
+
+        if (!only_local && !exists && url.find(".php") == std::string::npos && !domain.empty())
         {
             links.insert(std::make_pair(url, root_url));
         }
@@ -122,7 +124,7 @@ static size_t cb(char *data, size_t size, size_t nmemb, void *userdata)
     std::memcpy(receivedData, data, totalSize);
 
     WriteFunctionData *userData = (struct WriteFunctionData *)userdata;
-    std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> links = extract_links(receivedData, *(userData->checked_urls), userData->url, userData->only_local);
+    std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> links = extract_links(receivedData, userData->url, userData->only_local, userData->HashSet);
 
     userData->new_urls->insert(links.begin(), links.end());
 
@@ -146,12 +148,12 @@ static size_t cb(char *data, size_t size, size_t nmemb, void *userdata)
     return totalSize;
 }
 
-static void init(CURLM *cm, std::string url, std::string parent, StripedHashSet<Webpage> &HashSet, std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *checked_urls, std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *new_urls, bool only_local, int level)
+static void init(CURLM *cm, std::string url, std::string parent, StripedHashSet<Webpage> &HashSet,
+                 std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *new_urls, bool only_local, int level)
 {
     WriteFunctionData *data = new WriteFunctionData;
     data->url = url;
     data->HashSet = &HashSet;
-    data->checked_urls = checked_urls;
     data->new_urls = new_urls;
     data->parent = parent;
     data->only_local = only_local;
@@ -166,16 +168,15 @@ static void init(CURLM *cm, std::string url, std::string parent, StripedHashSet<
     curl_easy_setopt(eh, CURLOPT_PRIVATE, url.c_str());
     curl_easy_setopt(eh, CURLOPT_VERBOSE, 0L);
     curl_easy_setopt(eh, CURLOPT_FOLLOWLOCATION, 1L);
-    curl_easy_setopt(eh, CURLOPT_SSL_VERIFYHOST, 0); // this line
-    curl_easy_setopt(eh, CURLOPT_SSL_VERIFYPEER, 0); // and this line fixed the issue
+    curl_easy_setopt(eh, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_easy_setopt(eh, CURLOPT_SSL_VERIFYPEER, 0);
 
     curl_multi_add_handle(cm, eh);
 }
 
 // This is the function that each thread will run.
 void process_links(std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> urls, int num_urls, int max_wait, StripedHashSet<Webpage> &HashSet,
-                   std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *checked_urls, std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *current_urls,
-                   std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *new_urls, bool only_local, int maxlevel)
+                   std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *new_urls, bool only_local, int maxlevel, int num_links)
 {
     CURLM *cm = curl_multi_init();
     CURL *eh;
@@ -194,9 +195,10 @@ void process_links(std::unordered_set<std::pair<std::string, std::string>, Compa
             current_level = parent_webpage->current_level + 1;
         }
 
-        if (current_level <= maxlevel)
+        if (current_level <= maxlevel && counter <= num_links)
         {
-            init(cm, it->first, it->second, HashSet, checked_urls, new_urls, only_local, current_level);
+            init(cm, it->first, it->second, HashSet, new_urls, only_local, current_level);
+            counter.fetch_add(1, std::memory_order_relaxed);
         }
         else
         {
@@ -262,13 +264,11 @@ void process_links(std::unordered_set<std::pair<std::string, std::string>, Compa
 }
 
 void crawl_webpage(std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> urls, int num_urls, int max_wait,
-    StripedHashSet<Webpage> &HashSet, bool only_local = true, int max_links_per_thread = 50, int max_level = 3, int disregard_leftovers = false)
+                   StripedHashSet<Webpage> &HashSet, bool only_local = true, int max_links_per_thread = 50, int max_level = 3,
+                   int disregard_leftovers = false, int num_threads = 2, int num_links = 100)
 {
-    std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *checked_urls = new std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst>();
     std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *current_urls = new std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst>(urls);
     std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> *new_urls = new std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst>();
-
-    int num_threads = 2; // Number of threads to use
 
     // Create a vector to hold the thread objects
     std::vector<std::thread> threads;
@@ -276,11 +276,7 @@ void crawl_webpage(std::unordered_set<std::pair<std::string, std::string>, Compa
 
     while (true)
     {
-        std::cout << "Iteration: " << current_iteration << std::endl;
         current_iteration++;
-        // Add the urls we are about to check to the `checked_urls` set, so we do not treat them as new urls
-        checked_urls->insert(current_urls->begin(), current_urls->end());
-
         // Split the `current_urls` into `num_threads` parts
         int num_per_thread = std::min(static_cast<int>(current_urls->size() / num_threads + 1), max_links_per_thread);
 
@@ -289,9 +285,10 @@ void crawl_webpage(std::unordered_set<std::pair<std::string, std::string>, Compa
         // Create the threads
         for (int i = 0; i < num_threads; ++i)
         {
-            std::cout << "Thread: " << i << "Num per thread" << num_per_thread << std::endl;
-            // Determine the range of urls that this thread will process
+            // std::cout << "Thread: " << i << "Num per thread" << num_per_thread << std::endl;
+            //  Determine the range of urls that this thread will process
             auto next_iter = iter;
+
             std::advance(next_iter, std::min(num_per_thread, static_cast<int>(std::distance(iter, current_urls->end()))));
 
             // Create a new set for this range of urls
@@ -299,7 +296,7 @@ void crawl_webpage(std::unordered_set<std::pair<std::string, std::string>, Compa
             {
                 std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> thread_urls(iter, next_iter);
                 // Create and start the thread
-                threads.push_back(std::thread(process_links, thread_urls, num_urls, max_wait, std::ref(HashSet), checked_urls, current_urls, new_urls, only_local, max_level));
+                threads.push_back(std::thread(process_links, thread_urls, num_urls, max_wait, std::ref(HashSet), new_urls, only_local, max_level, num_links));
 
                 // Advance the iterator for the next thread
                 iter = next_iter;
@@ -316,6 +313,16 @@ void crawl_webpage(std::unordered_set<std::pair<std::string, std::string>, Compa
 
         threads.clear();
         // Merge the new urls into the current urls
+
+        if (iter == current_urls->end())
+        {
+            current_urls->empty();
+        }
+        else
+        {
+            std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> left_urls(iter, current_urls->end());
+            *current_urls = left_urls;
+        }
 
         if (disregard_leftovers)
         {
--- main.cpp
+++ main.cpp
@@ -7,37 +7,87 @@
 #include "hash_table.h"
 #include "webpage.h"
 #include "tests.h"
-#include <chrono> 
+#include <chrono>
+#include <stdlib.h> //for using the function sleep
+#include <fstream>
 
+std::ofstream file("output.txt", std::ios::app); // Open the file "output.txt" for writing
 
-
-int main()
+void execute_process(bool only_local = true, int max_links_per_thread = 50, int max_level = 3, int disregard_leftovers = false, int numthreads = 2, int num_links = 1000)
 {
+
     auto start = std::chrono::high_resolution_clock::now(); // Start the timer
 
-    //manualTestingHash();
+    // manualTestingHash();
 
     std::unordered_set<std::pair<std::string, std::string>, CompareFirst, EqualFirst> urls;
 
-    urls.insert(std::make_pair("http://en.wikipedia.org/wiki/Main_Page/", "")); //Initial link must be provided in this format
+    urls.insert(std::make_pair("http://en.wikipedia.org/wiki/Main_Page/", "")); // Initial link must be provided in this format
 
-    int num_urls =  4;
+    int num_urls = 4;
     int max_wait = 30 * 1000; /* Wait max. 30 seconds */
-    StripedHashSet<Webpage> HashSet(10);
+    StripedHashSet<Webpage> HashSet(100);
 
-    crawl_webpage(urls, num_urls, max_wait, HashSet);
-    //TestingLikeTutorial();
+    crawl_webpage(urls, num_urls, max_wait, HashSet, only_local, max_links_per_thread, max_level, disregard_leftovers, numthreads, num_links);
+    // TestingLikeTutorial();
 
-    HashSet.print_all();
-
+    // HashSet.print_all();
 
     int number = HashSet.get_count();
-    std::cout << number << std::endl;
 
     auto end = std::chrono::high_resolution_clock::now(); // Stop the timer
 
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
 
-    
+    if (file.is_open())
+    {
+        file << "--------------------------------------------" << std::endl;
+        file << "Elapsed time: " << elapsed.count() << " seconds, using " << numthreads << " threads" << std::endl;
+        file << "Links visited: " << number << ", Tried to visit:" << num_links << std::endl;
+        file << "Links per thread: " << max_links_per_thread << std::endl;
+        file << "--------------------------------------------" << std::endl;
+        std::cout << "Data written to the file." << std::endl;
+    }
+    else
+    {
+        std::cout << "Failed to open the file." << std::endl;
+    }
+}
+
+int main()
+{
+
+    int thread_numbers[] = {8, 16, 32};
+    int links_per_thread_numbers[] = {8, 16, 32, 64, 128};
+    int links_number[] = {10000};
+
+    int thread_numbers_size = sizeof(thread_numbers) / sizeof(thread_numbers[0]);
+    int links_per_thread_size = sizeof(links_per_thread_numbers) / sizeof(links_per_thread_numbers[0]);
+    int links_number_size = sizeof(links_number) / sizeof(links_number[0]);
+
+    for (int j = 0; j < links_per_thread_size; ++j)
+    {
+        for (int i = 0; i < thread_numbers_size; ++i)
+        {
+            for (int k = 0; k < links_number_size; ++k)
+            {
+                int num_threads = thread_numbers[i];
+                int link_per_thread = links_per_thread_numbers[j];
+                int num_links = links_number[k];
+
+                for (int m = 0; m < 3; ++m)
+                {
+                    if ((i != 1 || k != 10000) && (i != 1 || k != 5000) && (i != 2 || k != 10000))
+                    {
+
+                        execute_process(true, link_per_thread, 1000, false, num_threads, num_links);
+                        has_level_been_reached = false;
+                        counter = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    file.close();
 }
